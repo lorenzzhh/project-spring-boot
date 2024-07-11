1eb614f0cfb8ab66073ad74851eb0d30c001c652
lorenzzhh <lorenz.fritschi@gmx.ch>
2024-07-11 14:03:32 +0200

    other files

---
+++ src/test/java/ch/axa/projectspringboot/ProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.axa.projectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
